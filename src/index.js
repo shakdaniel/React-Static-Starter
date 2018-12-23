e155d4fed1785afc0d59101a48024c3dce7129d8
Shak <web.designs@me.com>
2018-12-23 13:18:17 -0500

    switching react-snapshot to react-snap to compare.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 // import { render } from "react-dom";
-import { render } from "react-snapshot";
+// import { render } from "react-snapshot";
+import { hydrate, render } from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+// render(<App />, document.getElementById("root"));
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 serviceWorker.register();
